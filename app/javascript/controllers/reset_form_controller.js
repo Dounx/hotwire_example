2dbe3db75440105950f91bccad8abf29bc4cb5cc
Dounx <imdounx@gmail.com>
2021-03-16 16:00:25 +0800

    Fix Stimulus

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,15 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("Connected")
-  }
-
   reset() {
     this.element.reset()
-  }
-
-  hello() {
-    console.log("Hello")
   }
 }
