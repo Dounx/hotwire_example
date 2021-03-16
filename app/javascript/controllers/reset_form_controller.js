0fc80f91fd144c0f11d24839c09461fd0a4da175
Dounx <imdounx@gmail.com>
2021-03-16 15:17:57 +0800

    Use turbo and remove useless views

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  connect() {
+    console.log("Connected")
+  }
+
   reset() {
     this.element.reset()
+  }
+
+  hello() {
+    console.log("Hello")
   }
 }
